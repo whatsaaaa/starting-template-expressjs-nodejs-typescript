44bfde32913029bb3d0ddc6e7187d157bd549938
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-10-31 18:18:19 +0100

    Basic express app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
+import express from "express";
 /**
  * EXPRESSJS NODEJS TYPESCRIPT Template
  * ----------------------------------------------
  */
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Express + Node + Typescript Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
